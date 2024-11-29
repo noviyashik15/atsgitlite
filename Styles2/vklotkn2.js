89d70d21c4cad27523caf1f2cede683ceb70adfe
ItsMyName44 <Noviyashik15@yandex.ru>
2024-11-29 19:12:40 +0300

    sec4lite commit

---
+++ Styles2/vklotkn2.js
@@ -0,0 +1,27 @@
+$("#viklbut").on("click", function () {
+   $("#vklbut").removeAttr("disabled");
+   $("#viklbut").attr("disabled", "disabled");
+   document.getElementById("iguasu1").remove();
+   document.getElementById("angel1").remove();
+   document.getElementById("victory1").remove();
+   document.getElementById("niagara1").remove();
+   document.getElementById("kanion1").remove();
+   document.getElementById("yellow1").remove();
+   
+   document.getElementById("play7").remove();  
+
+   document.getElementById("halong").remove();
+
+   document.getElementById("dubai").remove();  
+   
+   document.getElementById("ozera").remove();  
+
+   document.getElementById("chzan").remove();  
+
+   // document.getElementById("oglavn1").remove();
+   // document.getElementById("oglavn2").remove();
+   // document.getElementById("rubai1").remove();
+
+   document.getElementById("zaviruh1").remove();
+});
+        
